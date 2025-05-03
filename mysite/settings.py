8bb73053034ab0b2aa245b84655524811e82d968
Paul Sommers <psommers1@ivytech.edu>
2025-05-03 09:38:38 -0400

    Homework: Adding security to your website

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login redirect
+LOGIN_REDIRECT_URL = '/'
